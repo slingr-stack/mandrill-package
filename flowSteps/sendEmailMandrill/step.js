933732338ae2931918f180cadf51fcf93db44c67
Angel Perez Personal <pasaperez@yahoo.com.ar>
2023-11-13 16:57:52 -0300

    Remove helpers

--- flowSteps/sendEmailMandrill/step.js
+++ flowSteps/sendEmailMandrill/step.js
@@ -4,10 +4,9 @@
 
 var httpService = dependencies.http;
 
-
 step.sendEmailMandrill = function (inputs) {
 
-	var inputs = {
+	var inputsLogic = {
 		from: inputs.from || "",
 		subject: inputs.subject || "",
 		message: inputs.message || "",
@@ -17,14 +16,14 @@ step.sendEmailMandrill = function (inputs) {
 	var msg = {
 		"key": config.get("apiKey"),
 		"message": {
-			"from_name": inputs.from,
-			"subject": inputs.subject,
-			"html": inputs.message,
+			"from_name": inputsLogic.from,
+			"subject": inputsLogic.subject,
+			"html": inputsLogic.message,
 			"from_email": config.get("account"),
 			"headers":{},
 			"to": [
 				{
-					"email": inputs.to,
+					"email": inputsLogic.to,
 					"type": "to"
 				}
 			]
@@ -36,6 +35,36 @@ step.sendEmailMandrill = function (inputs) {
 		body: msg
 	}
 
+	options= setRequestHeaders(options);
+	options = setRequestBody(options);
+
 	return httpService.post(options);
 
 };
+
+function setRequestHeaders(options) {
+	var headers = options.headers || {};
+	headers = mergeJSON(headers, {"Content-Type": "application/json"});
+
+	options.headers = headers;
+	return options;
+}
+
+function setRequestBody(options) {
+	var body = options.body || {};
+	body.key = body.key || config.get("apiKey");
+	options.body = body;
+	return options;
+}
+
+function mergeJSON (json1, json2) {
+	var result = {};
+	var key;
+	for (key in json1) {
+		if(json1.hasOwnProperty(key)) result[key] = json1[key];
+	}
+	for (key in json2) {
+		if(json2.hasOwnProperty(key)) result[key] = json2[key];
+	}
+	return result;
+}

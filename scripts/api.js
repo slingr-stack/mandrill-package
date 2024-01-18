7fbd1c0b5903547946c51745fc633d96e1afb142
pasaperez <aperez@slingr.io>
2024-01-18 00:48:11 -0300

    function

--- scripts/api.js
+++ scripts/api.js
@@ -260,6 +260,21 @@ exports.utils.verifySignature = function (body, signature, signature256) {
  Private helpers
  ****************************************************/
 
+function parse (url, pathVariables){
+    var regex = /{([^}]*)}/g;
+    if (!url.match(regex)){
+        return url;
+    }
+    if(!pathVariables){
+        sys.logs.error('No path variables have been received and the url contains curly brackets\'{}\'');
+        throw new Error('Error please contact support.');
+    }
+    url = url.replace(regex, function(m, i) {
+        return pathVariables[i] ? pathVariables[i] : m;
+    })
+    return url;
+}
+
 function checkHttpOptions (path, options) {
     options = options || {};
     if (!!path) {
